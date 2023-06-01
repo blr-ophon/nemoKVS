bd993a9a3c2ff8ad724eb788f0bd8f206186d5b5
blrophon <gabriel.pequod@gmail.com>
2023-06-01 11:18:54 -0300

    Add kvpair encode/decode

--- include/kvpair.h
+++ include/kvpair.h
@@ -18,7 +18,7 @@ void KVpair_free(KVpair *ptr);
 uint32_t KVpair_getKey(KVpair *ptr);        //string to uint32t
 size_t KVpair_getSize(KVpair *ptr);        //string to uint32t
 
-KVpair *KVpair_decode(uint8_t *offset);     //convert byte array to KV pair
+KVpair *KVpair_decode(uint8_t *bytestream);     //convert byte array to KV pair
 uint8_t *KVpair_encode(KVpair *kv);         //convert KV pair to byte array
 
 #endif
--- src/dstructs/kvpair.c
+++ src/dstructs/kvpair.c
@@ -1,5 +1,40 @@
 #include "kvpair.h"
 
+
+KVpair *KVpair_decode(uint8_t *bytestream){ 
+    uint32_t klen;
+    memcpy(&klen, bytestream, sizeof(uint32_t));
+    bytestream += sizeof(uint32_t);
+
+    uint32_t vlen;
+    memcpy(&vlen, bytestream, sizeof(uint32_t));
+    bytestream += sizeof(uint32_t);
+
+    char *key = malloc(klen); 
+    memcpy(key, bytestream, klen);
+    bytestream += klen;
+
+    uint8_t *val= malloc(vlen); 
+    memcpy(val, bytestream, vlen);
+
+    KVpair *kv = KVpair_create(klen, vlen, key, val);
+    free(key);
+    free(val);
+    return kv;
+}
+                                           
+uint8_t *KVpair_encode(KVpair *kv){
+    size_t streamSize = 2*sizeof(uint32_t) + kv->klen + kv->vlen;
+    uint8_t *bytestream = malloc(streamSize);
+
+    memcpy(bytestream, &kv->klen, sizeof(uint32_t));
+    memcpy(bytestream, &kv->vlen, sizeof(uint32_t));
+    memcpy(bytestream, kv->key, kv->klen);
+    memcpy(bytestream, kv->val, kv->vlen);
+
+    return bytestream;
+}
+
 KVpair *KVpair_create(uint32_t klen, uint32_t vlen, char *key, void *val){
     KVpair *kv = malloc(sizeof(KVpair));
 
@@ -28,3 +63,4 @@ uint32_t KVpair_getKey(KVpair *ptr){
 size_t KVpair_getSize(KVpair *ptr){
     return 2*sizeof(uint32_t) + ptr->klen + ptr->vlen;
 }
+
