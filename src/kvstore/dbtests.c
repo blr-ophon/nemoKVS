8957839cd9049d0549a2457d0ef33d73b8e7ded1
blrophon <gabriel.pequod@gmail.com>
2023-06-11 10:25:09 -0300

    Update dbtests. Test deletion special cases

--- src/kvstore/dbtests.c
+++ src/kvstore/dbtests.c
@@ -5,10 +5,76 @@
 //ERROR: degree 4, n = 10
 //16, 19, 16, 10, 19, 28, 1, 28, 25, 28
 
+void DBtests_custom(BPtree *tree){
+    BPtreeNode *root = tree->root;
+    int n = 10;
+    KVpair **KVs = malloc(n*sizeof(void*));
+    int i;
+    KVs[0] = KVpair_create(2, 10, "16", "testVal-16");
+    KVs[1] = KVpair_create(2, 10, "19", "testVal-19");
+    KVs[2] = KVpair_create(2, 10, "16", "testVal-16");
+    KVs[3] = KVpair_create(2, 10, "10", "testVal-10");
+    KVs[4] = KVpair_create(2, 10, "19", "testVal-19");
+    KVs[5] = KVpair_create(2, 10, "28", "testVal-28");
+    KVs[6] = KVpair_create(1, 9, "1", "testVal-1");
+    KVs[7] = KVpair_create(2, 10, "28", "testVal-28");
+    KVs[8] = KVpair_create(2, 10, "25", "testVal-25");
+    KVs[9] = KVpair_create(2, 10, "28", "testVal-28");
+
+    printf("%d sequential key-value pairs created for B+tree of degree %d\n",
+            10 , tree->degree);
+
+    //insert
+    for(i = 0; i < n; i++){
+        BPtree_insert(tree, KVs[i]);
+    }
+    printf("%d key-value pairs successfully inserted (IN ORDER)\n", i);
+    DBtests_search(tree, KVs, n);
+
+    //delete
+    for(i = 0; i < n; i++){
+        BPtree_delete(tree, KVs[i]);
+    }
+    printf("%d key-value pairs successfully deleted (IN ORDER)\n", i);
+
+    //try search
+    if(DBtests_search(tree, KVs, n)){
+        printf("Keys: \n");
+        for(i = 0; i < n; i++){
+            printf("%s ", KVs[i]->key);
+        }
+        printf("\n");
+    }
+
+    //free
+    for(int i = 0; i < n; i++){
+        KVpair_free(KVs[i]);
+    }
+    free(KVs);
+    printf("\n");
+}
+
+int DBtests_search(BPtree *tree, KVpair **KVs, int n){
+    int i;
+    //try search
+    int search_kv_n = 0;
+    for(i = 0; i < n; i++){
+        if(BPtree_search(tree, KVs[i], NULL)){
+            search_kv_n++;
+            //printf("KV[%d] found\n", i);
+        }
+    }
+    printf("Search complete: ");
+    printf("%d key-value pairs found\n", search_kv_n);
+
+    return search_kv_n;
+}
+
 void DBtests_all(BPtree *tree, int n){
-    DBtests_inorder(tree, n);
-    DBtests_revorder(tree, n);
-    DBtests_randorder(tree, n);
+    DBtests_custom(tree);
+    //DBtests_inorder(tree, n);
+    //DBtests_revorder(tree, n);
+    //DBtests_randorder(tree, n);
 }
 
 void DBtests_randorder(BPtree *tree, int n){
@@ -33,6 +99,7 @@ void DBtests_randorder(BPtree *tree, int n){
         BPtree_insert(tree, KVs[i]);
     }
     printf("%d key-value pairs successfully inserted\n", i);
+    DBtests_search(tree, KVs, n);
 
     //delete
     for(i = 0; i < n; i++){
@@ -41,16 +108,7 @@ void DBtests_randorder(BPtree *tree, int n){
     printf("%d key-value pairs successfully deleted\n", i);
 
     //try search
-    int search_kv_n = 0;
-    for(i = 0; i < n; i++){
-        if(BPtree_search(tree, KVs[i], NULL)){
-            search_kv_n++;
-            printf("KV[%d] found\n", i);
-        }
-    }
-    printf("Search complete\n");
-    printf("%d key-value pairs found\n\n", search_kv_n);
-    if(search_kv_n){
+    if(DBtests_search(tree, KVs, n)){
         printf("Keys: \n");
         for(i = 0; i < n; i++){
             printf("%s ", KVs[i]->key);
@@ -65,7 +123,7 @@ void DBtests_randorder(BPtree *tree, int n){
         KVpair_free(KVs[i]);
     }
     free(KVs);
-
+    printf("\n");
 }
 
 void DBtests_inorder(BPtree *tree, int n){
@@ -88,6 +146,7 @@ void DBtests_inorder(BPtree *tree, int n){
         BPtree_insert(tree, KVs[i]);
     }
     printf("%d key-value pairs successfully inserted (IN ORDER)\n", i);
+    DBtests_search(tree, KVs, n);
 
     //delete
     for(i = 0; i < n; i++){
@@ -96,20 +155,20 @@ void DBtests_inorder(BPtree *tree, int n){
     printf("%d key-value pairs successfully deleted (IN ORDER)\n", i);
 
     //try search
-    int search_kv_n = 0;
-    for(i = 0; i < n; i++){
-        if(BPtree_search(tree, KVs[i], NULL)){
-            search_kv_n++;
+    if(DBtests_search(tree, KVs, n)){
+        printf("Keys: \n");
+        for(i = 0; i < n; i++){
+            printf("%s ", KVs[i]->key);
         }
+        printf("\n");
     }
-    printf("Search complete\n");
-    printf("%d key-value pairs found\n\n", search_kv_n);
 
     //free
     for(int i = 0; i < n; i++){
         KVpair_free(KVs[i]);
     }
     free(KVs);
+    printf("\n");
 }
 
 void DBtests_revorder(BPtree *tree, int n){
@@ -132,25 +191,28 @@ void DBtests_revorder(BPtree *tree, int n){
         BPtree_insert(tree, KVs[n-1 - i]);
     }
     printf("%d key-value pairs successfully inserted (IN REVERSE ORDER)\n", i);
+
+    DBtests_search(tree, KVs, n);
+
     //delete
     for(i = 0; i < n; i++){
         BPtree_delete(tree, KVs[n-1 - i]);
     }
     printf("%d key-value pairs successfully deleted (IN REVERSE ORDER)\n", i);
-    //try search
-    int search_kv_n = 0;
-    for(i = 0; i < n; i++){
-        if(BPtree_search(tree, KVs[i], NULL)){
-            search_kv_n++;
-        }
-    }
-    printf("Search complete\n");
-    printf("%d key-value pairs found\n\n", search_kv_n);
 
+    //search
+    if(DBtests_search(tree, KVs, n)){
+        printf("Keys: \n");
+        for(i = 0; i < n; i++){
+            printf("%s ", KVs[i]->key);
+        }
+        printf("\n");
+    }
 
     //free
     for(int i = 0; i < n; i++){
         KVpair_free(KVs[i]);
     }
     free(KVs);
+    printf("\n");
 }
